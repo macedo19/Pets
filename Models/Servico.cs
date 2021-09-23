5d8d6595a574370b8ffb460bf265ac2095cda2bc
FelipeMacchiz <felipe.macchiz@gmail.com>
2021-09-23 19:06:33 -0300

    Servicos Controller
    Fazendo as funções do CRUD dos Serviços

--- Models/Servico.cs
+++ Models/Servico.cs
@@ -1,13 +1,10 @@
 using System;
 
-namespace EcommerceAPI.Models{
-
+namespace EcommerceAPI.Models {
     public class Servico {
-
         public int Id { get; set; }
         public int CPF { get; set; }
         public DateTime Data { get; set; }
         public string Descricao { get; set; }
-
     }
 }
