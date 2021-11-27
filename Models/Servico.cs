43cd3b232761ae10fe54a6685059f0f1d110cf85
Ana Maria Vargas <aninha_vargas@hotmail.com>
2021-11-27 16:45:22 -0300

    inclusao relacionamentos

--- Models/Servico.cs
+++ Models/Servico.cs
@@ -8,7 +8,8 @@ namespace EcommerceAPI.Models
         public int Id { get; set; }
         public long CPF { get; set; }
         public DateTime Data { get; set; }
-
+        public Cliente Cliente { get; set; }
+        public Horarios Horario { get; set; }
         public int Hora { get; set; }
         public string Descricao { get; set; }
     }
